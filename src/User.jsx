21238e4c14da5583a1a02b23a936083fdd10861c
bethwelmusin <betwelmusin@gmail.com>
2024-08-30 14:14:42 +0300

    btn fix

--- src/User.jsx
+++ src/User.jsx
@@ -57,7 +57,11 @@ const User = () => {
           </Box>
 
           <Box sx={{ display: 'flex', justifyContent: 'space-around', mt: 2 }}>
-            <Button variant="contained" color="success" sx={{ width: '45%' }}>
+            <Button variant="contained" color="success"   sx={{ 
+    width: '48%',  
+    backgroundColor: '#1976D2', 
+    color: 'white' 
+  }}>
               Accept
             </Button>
             <Button variant="contained" color="error" sx={{ width: '45%' }}>
